030d4f6ace723d9bdad8c108c8c0e6e19184a2c9
Tony <1206969574@qq.com>
2024-05-17 11:28:16 +0800

    Create Base Character

---
+++ Source/GasStudyProject/Public/Character/AuruCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuruCharacterBase.h"
+#include "AuruCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASSTUDYPROJECT_API AAuruCharacter : public AAuruCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GasStudyProject/Public/Character/AuruEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuruCharacterBase.h"
+#include "AuruEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASSTUDYPROJECT_API AAuruEnemy : public AAuruCharacterBase
+{
+	GENERATED_BODY()
+	
+};
